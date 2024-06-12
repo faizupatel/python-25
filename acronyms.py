389db1cb81fbc3acc508ccab9ee657966597c3be
faizan patel <faizupatel0025@gmail.com>
2024-06-12 20:52:37 +0530

    ACRONYMS
    this is a mini code

---
+++ acronyms.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      Faizan
+#
+# Created:     12/06/2024
+# Copyright:   (c) Faizan 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+user_input = str(input("Enter a Phrase: "))
+text = user_input.split()
+a = " "
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)f
